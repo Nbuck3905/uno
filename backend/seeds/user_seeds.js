070b15c3b187108646e720d17c07e8de002ad56c
Joseph Burks <joe_burks_3@yahoo.com>
2020-01-17 09:31:10 -0600

    added backend

---
+++ backend/seeds/user_seeds.js
@@ -0,0 +1,10 @@
+
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('users').del()
+      // Inserts seed entries
+  await knex('users').insert([
+    {username: 'jobu', password: '1'},
+    {username: 'nbuch', password: '1'}
+  ]);
+};
