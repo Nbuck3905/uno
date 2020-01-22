c27215cc84580c8567ce2f62afef65c837d8785e
Nbuck3905 <Nathanbuchanan671@gmail.com>
2020-01-22 15:58:33 -0600

    added shuffle

--- front-end/src/Components/GamePlay.js
+++ front-end/src/Components/GamePlay.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import { io } from '../Socket'
+import React from "react";
+import { io } from "../Socket";
+import UnoCard from "./UnoCard";
+import "../UnoCard.css";
 
-export default class GamePlay extends React.Component{
-    render(){
-        return(
-            <div>
-                
-            </div>
-        )
-    }
+export default class GamePlay extends React.Component {
+  render() {
+    console.log(this.props.drawCards);
+    return (
+      <div>
+        {this.props.cards.map(card => (
+          <UnoCard {...card} />
+        ))}
+      </div>
+    );
+  }
 }
